3629756d5fd55cdc3d199edca022f3cc902c6ed2
Megan Beckett <megan.beckett13@gmail.com>
2019-06-20 17:23:40 +0200

    Slides to pdf

---
+++ slides_to_pdf.R
@@ -0,0 +1,13 @@
+library(pagedown)
+library(here)
+
+url <- here::here("userR_2019/useR_2019_osrm.html")
+pagedown::chrome_print(url, format = "pdf")
+
+
+library(webshot)
+install_phantomjs()
+
+file_name <- paste0("file://", normalizePath(here::here("userR_2019/useR_2019_osrm.html")))
+
+webshot(url, "user_2019_mbeckett.pdf")
